3e792ff36cb8ee9abb651a5b0f5c7447f2c5ce36
erikzaksf <erik.zak.sf@gmail.com>
2017-12-06 09:26:43 -0800

    add games to routes file and add show page for games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,5 +3,7 @@ class GamesController < ActionController::Base
     @games = Game.all
     # @order_item = current_order.order_items.new     LATER ON MAKE FUNCTIONAL!!!!
   end
-
+  def show
+    @game = Game.find(params[:id])
+  end
 end
