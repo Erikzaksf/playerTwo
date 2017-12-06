b94a268dada3c095067a3056d6004bb0e3142c02
erikzaksf <erik.zak.sf@gmail.com>
2017-12-06 08:27:05 -0800

    add games model and controller

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,7 @@
+class GamesController < ActiveRecord::Base
+  def index
+    @games = Game.all
+    # @order_item = current_order.order_items.new     LATER ON MAKE FUNCTIONAL!!!!
+  end
+
+end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,3 @@
-
 class Game < ActiveRecord::Base
   validates :name, :presence => true
   validates :details, :presence => true
